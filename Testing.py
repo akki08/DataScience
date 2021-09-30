dabd70e41122ac2d8e4ad5351e833d75b7ca5f96
akki08 <akki08>
2021-09-30 08:37:13 +0000

    testing

---
+++ Testing.py
@@ -0,0 +1,14 @@
+# Databricks notebook source
+import pandas as pd
+
+# COMMAND ----------
+
+df = spark.sql("select * from consumer_insights_internal.abtests_master_table limit 10")
+
+# COMMAND ----------
+
+display(df)
+
+# COMMAND ----------
+
+
